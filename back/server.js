c68d61367e5bf805efcd635d01b56d9c96feecff
luxeivan <luxeivan@gmail.com>
2022-07-13 22:52:07 +0300

    setting back

--- back/server.js
+++ back/server.js
@@ -1,11 +1,50 @@
-import express from "express";
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const messages = [];
+const messages = [{
+    type: 'text',
+    message: 'lorem ipsum',
+    date: '11-07-2022 10:30',
+    data: 'dasdasdasdasdasdasdasdasdasdasd'
+},
+{
+    type: 'audio',
+    message: 'audio',
+    date: '13-07-2022 21:30',
+    data: 'jhejrghvjldknehfgiejdflkbmvsdlkgo'
+},
+{
+    type: 'image',
+    message: 'my first image',
+    date: '13-07-2022 20:30',
+    data: 'dasdasdasdasdasdasdasdasdasdasd'
+},
+{
+    type: 'audio',
+    message: 'audio',
+    date: '12-07-2022 22:30',
+    data: 'jhejrghvjldknehfgiejdflkbmvsdlkgo'
+}];
 
+app.use(cors());
 app.listen(3000, () => {
     console.log('server start to port 3000');
 });
 
-app.get("/", (req, res) => {
-    res.send('12345678');
+app.get("/all", (req, res) => {
+    res.send(messages);
 });
+app.get("/messages", (req, res) => {
+    console.log(req.query.searchtext);
+    res.send(messages.filter(elem => {
+        return elem.message.includes(req.query.searchtext) || elem.date.includes(req.query.searchtext)
+    }));
+});
+
+function dateConvert(unix_timestamp){
+    let date = new Date(unix_timestamp * 1000);
+    let hours = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    let seconds = "0" + date.getSeconds();
+    return formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+}
--- front/src/js/Chat.js
+++ front/src/js/Chat.js
@@ -1,31 +1,100 @@
+import { merge, fromEvent, debounceTime, distinctUntilChanged, EMPTY } from 'rxjs';
+import { map, switchMap, tap, catchError } from 'rxjs/operators'
+import { ajax } from 'rxjs/ajax'
 
-import { fromEvent } from 'rxjs';
-import { map,tap } from 'rxjs/operators'
 export default class Chat {
     constructor() {
         console.log('run chat');
+        this.url = 'http://localhost:3000';
         this.inputText = document.getElementById('input_text');
+        this.inputSearch = document.querySelector('.chat__search-input');
+        this.chatList = document.querySelector('.chat__list');
+        this.chatListArea = document.querySelector('.chat__list-area');
         this.init();
     }
-    init(){
+    init() {
+        window.onload = this.scrollDown()
+
+        //Подписка на ввод текста в основном инпуте
         fromEvent(document.querySelector('.chat__form'), 'submit')
-        .pipe(
-            tap(event=>event.preventDefault()),
-            map(event => this.inputText.value)
-        ).subscribe(text=>{
-            this.inputText.value = '';
-            console.log(text);
-        });
+            .pipe(
+                tap(event => event.preventDefault()),
+                map(event => this.inputText.value)
+            ).subscribe(() => {
+                if (this.inputText.value) this.addMessageToChatList(this.inputText.value);
+                this.inputText.value = '';
+            });
 
-        fromEvent(document.querySelector('.chat__search'), 'click')        
-        .subscribe(event=>{
-            console.log('Search');
-        });
+        //подписка на событие поиска ввод текста и кнопка поиска
+        merge(
+            fromEvent(document.querySelector('.chat__search'), 'click'),
+            fromEvent(document.querySelector('.chat__search-input'), 'input')
+                .pipe(      
+                    map(event => event.target.value),             
+                    debounceTime(1000),
+                    distinctUntilChanged()
+                )
+        ).pipe(map(event => this.inputSearch.value),switchMap(this.getSearchMessages.bind(this)))
+            .subscribe(res => {
+                // if (this.inputSearch.value)
+                //     console.log('Search: ', this.inputSearch.value);
+                console.log(res);
+                this.renderMessages(res);
+            },
+                error => {
+                    console.log(error);
+                });
 
-        fromEvent(document.querySelector('.attach-file'), 'click')        
-        .subscribe(event=>{
-            console.log('Attach');
-        });
+        //подписка на события прикрепления файла
+        fromEvent(document.querySelector('.attach-file'), 'click')
+            .subscribe(event => {
+                console.log('Attach');
+            });
     }
-    
+    scrollDown() {
+        this.chatListArea.scrollTop = this.chatListArea.scrollHeight - this.chatListArea.clientHeight;
+    }
+    dateNow() {
+        let a = new Date();
+        return `${a.getDate()}-${a.getMonth() < 10 ? 0 + (a.getMonth() + 1).toString() : a.getMonth() + 1}-${a.getFullYear()} ${a.getHours()}:${a.getMinutes()}`;
+    }
+
+    addMessageToChatList(message, type = 'text') {
+        this.chatList.insertAdjacentHTML('beforeend', `
+        <li class="chat__item">
+        <div class="chat__item-content">${message}</div>  
+        <div class="chat__item-time">${this.dateNow()}</div></li>`);
+        this.scrollDown();
+    }
+
+    getTenMessages() {
+        let first = 0;
+        return ajax.getJSON(`http://localhost:3000/all?first=${first}`)
+            .pipe(
+                catchError(err => {
+                    console.log(err);
+                    return EMPTY;
+                }));
+    }
+    getSearchMessages(text) {
+        return ajax.getJSON('http://localhost:3000/messages?searchtext=' + text)
+            .pipe(
+                catchError(err => {
+                    console.log(err);
+                    return EMPTY;
+                }));
+    }
+
+    renderMessages(messages) {
+        this.chatList.innerHTML = '';
+        messages.forEach(message=>{
+            this.chatList.insertAdjacentHTML('beforeend', `
+            <li class="chat__item">
+            <div class="chat__item-content">${message.message}</div>  
+            <div class="chat__item-time">${message.date}</div></li>`);
+        });
+        
+        this.scrollDown();
+    }
+
 }
