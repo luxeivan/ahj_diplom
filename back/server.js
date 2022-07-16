ee83e19fda8eb7072defcdd98b008d22ad85f896
luxeivan <luxeivan@gmail.com>
2022-07-16 23:28:29 +0300

    add new function

--- back/server.js
+++ back/server.js
@@ -1,47 +1,128 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 const messages = [{
     type: 'text',
-    message: 'lorem ipsum',
+    message: 'Один',
     date: '11-07-2022 10:30',
     data: 'dasdasdasdasdasdasdasdasdasdasd'
 },
 {
-    type: 'audio',
-    message: 'audio',
+    type: 'text',
+    message: 'Два',
     date: '13-07-2022 21:30',
     data: 'jhejrghvjldknehfgiejdflkbmvsdlkgo'
 },
 {
-    type: 'image',
-    message: 'my first image',
+    type: 'text',
+    message: 'Три',
     date: '13-07-2022 20:30',
     data: 'dasdasdasdasdasdasdasdasdasdasd'
 },
 {
-    type: 'audio',
-    message: 'audio',
+    type: 'text',
+    message: 'Четыре',
     date: '12-07-2022 22:30',
     data: 'jhejrghvjldknehfgiejdflkbmvsdlkgo'
-}];
+},
+{
+    type: 'text',
+    message: 'Пять',
+    date: '11-07-2022 10:30',
+    data: 'dasdasdasdasdasdasdasdasdasdasd'
+},
+{
+    type: 'text',
+    message: 'Шесть',
+    date: '13-07-2022 21:30',
+    data: 'jhejrghvjldknehfgiejdflkbmvsdlkgo'
+},
+{
+    type: 'text',
+    message: 'Семь',
+    date: '11-07-2022 10:30',
+    data: 'dasdasdasdasdasdasdasdasdasdasd'
+},
+{
+    type: 'text',
+    message: 'Восемь',
+    date: '13-07-2022 21:30',
+    data: 'jhejrghvjldknehfgiejdflkbmvsdlkgo'
+}, {
+    type: 'text',
+    message: 'Девять',
+    date: '13-07-2022 21:30',
+    data: 'jhejrghvjldknehfgiejdflkbmvsdlkgo'
+},
+{
+    type: 'text',
+    message: 'Десять',
+    date: '13-07-2022 20:30',
+    data: 'dasdasdasdasdasdasdasdasdasdasd'
+},
+{
+    type: 'text',
+    message: 'Одиннадцать',
+    date: '12-07-2022 22:30',
+    data: 'jhejrghvjldknehfgiejdflkbmvsdlkgo'
+},
+{
+    type: 'text',
+    message: 'Двенадцать',
+    date: '11-07-2022 10:30',
+    data: 'dasdasdasdasdasdasdasdasdasdasd'
+},
+{
+    type: 'text',
+    message: 'Тринадцать',
+    date: '13-07-2022 21:30',
+    data: 'jhejrghvjldknehfgiejdflkbmvsdlkgo'
+},
+{
+    type: 'text',
+    message: 'Четырнадцать',
+    date: '11-07-2022 10:30',
+    data: 'dasdasdasdasdasdasdasdasdasdasd'
+},
+{
+    type: 'text',
+    message: 'Пятнадцать',
+    date: '13-07-2022 21:30',
+    data: 'jhejrghvjldknehfgiejdflkbmvsdlkgo'
+},];
 
+app.use(bodyParser.json({ limit: '20mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '20mb' }));
+
+const jsonParser = express.json();
 app.use(cors());
 app.listen(3000, () => {
     console.log('server start to port 3000');
 });
 
-app.get("/all", (req, res) => {
-    res.send(messages);
-});
+// app.get("/all", (req, res) => {
+//     res.send(messages.slice(Math.max(messages.length - 10 - req.query.first, 0), Math.max(messages.length - req.query.first,0)).reverse());
+// });
 app.get("/messages", (req, res) => {
-    console.log(req.query.searchtext);
-    res.send(messages.filter(elem => {
-        return elem.message.includes(req.query.searchtext) || elem.date.includes(req.query.searchtext)
-    }));
+    let searchText = req.query.searchtext.toLowerCase();
+    let first = req.query.first;
+    let afterfilter = messages.filter(elem => { return elem.message.toLowerCase().includes(searchText) || elem.date.includes(searchText) });
+    let afterslice = afterfilter.slice(Math.max(afterfilter.length - 10 - first, 0), Math.max(afterfilter.length - first, 0)).reverse()
+    res.send(afterslice);
+});
+app.post("/add", jsonParser, (req, res) => {
+    messages.push({
+        type: req.body.type,
+        message: req.body.message,
+        date: req.body.date,
+        data: req.body.data
+    })
+    res.send({ ok: true });
 });
 
-function dateConvert(unix_timestamp){
+function dateConvert(unix_timestamp) {
     let date = new Date(unix_timestamp * 1000);
     let hours = date.getHours();
     let minutes = "0" + date.getMinutes();
--- front/src/js/Chat.js
+++ front/src/js/Chat.js
@@ -1,19 +1,25 @@
 import { merge, fromEvent, debounceTime, distinctUntilChanged, EMPTY } from 'rxjs';
-import { map, switchMap, tap, catchError } from 'rxjs/operators'
+import { map, switchMap, tap, catchError, filter } from 'rxjs/operators'
 import { ajax } from 'rxjs/ajax'
 
 export default class Chat {
     constructor() {
-        console.log('run chat');
         this.url = 'http://localhost:3000';
         this.inputText = document.getElementById('input_text');
         this.inputSearch = document.querySelector('.chat__search-input');
         this.chatList = document.querySelector('.chat__list');
         this.chatListArea = document.querySelector('.chat__list-area');
+        this.addFile = document.getElementById('add-file');
         this.init();
     }
     init() {
-        window.onload = this.scrollDown()
+        this.getTenMessages().subscribe(res => {
+            this.renderMessages(res);
+        },
+            error => {
+                console.log(error);
+            });
+
 
         //Подписка на ввод текста в основном инпуте
         fromEvent(document.querySelector('.chat__form'), 'submit')
@@ -21,7 +27,8 @@ export default class Chat {
                 tap(event => event.preventDefault()),
                 map(event => this.inputText.value)
             ).subscribe(() => {
-                if (this.inputText.value) this.addMessageToChatList(this.inputText.value);
+                if (this.inputText.value) this.addMessageToServer(this.inputText.value);
+                //this.addMessageToChatList(this.inputText.value);
                 this.inputText.value = '';
             });
 
@@ -29,37 +36,58 @@ export default class Chat {
         merge(
             fromEvent(document.querySelector('.chat__search'), 'click'),
             fromEvent(document.querySelector('.chat__search-input'), 'input')
-                .pipe(      
-                    map(event => event.target.value),             
+                .pipe(
+                    map(event => event.target.value),
                     debounceTime(1000),
                     distinctUntilChanged()
                 )
-        ).pipe(map(event => this.inputSearch.value),switchMap(this.getSearchMessages.bind(this)))
+        ).pipe(map(event => this.inputSearch.value), switchMap((text)=>{return this.getTenMessages(0,text)}))
             .subscribe(res => {
-                // if (this.inputSearch.value)
-                //     console.log('Search: ', this.inputSearch.value);
-                console.log(res);
-                this.renderMessages(res);
+                this.renderMessages(res, true, 'afterbegin', true);
             },
                 error => {
                     console.log(error);
                 });
 
-        //подписка на события прикрепления файла
+        //подписка на события клика по иконке прикрепления файла
         fromEvent(document.querySelector('.attach-file'), 'click')
             .subscribe(event => {
-                console.log('Attach');
+                this.addFile.click()
             });
-    }
-    scrollDown() {
-        this.chatListArea.scrollTop = this.chatListArea.scrollHeight - this.chatListArea.clientHeight;
-    }
-    dateNow() {
-        let a = new Date();
-        return `${a.getDate()}-${a.getMonth() < 10 ? 0 + (a.getMonth() + 1).toString() : a.getMonth() + 1}-${a.getFullYear()} ${a.getHours()}:${a.getMinutes()}`;
+
+        //подписка на события прикрепления файла
+        fromEvent(this.addFile, 'change')
+            .subscribe(event => {
+                if (!event.target.files.length) return;
+                const reader = new FileReader()
+                reader.onload = file => {
+                    this.addMessageToServer(event.target.files[0].name, event.target.files[0].type, file.target.result)
+                }
+
+                reader.readAsDataURL(event.target.files[0]);
+            })
+
+        //подписка на события скрола сообщений
+        fromEvent(this.chatListArea, 'scroll')
+            .pipe(
+                filter(event => this.chatListArea.scrollTop === 0),
+                map(event => document.getElementsByClassName('chat__item').length),
+                switchMap((first)=>{return this.getTenMessages(first,this.inputSearch.value)}),
+                filter(arr => arr.length > 0)
+            )
+            .subscribe(res => {
+                this.renderMessages(res, false);
+            })
     }
 
-    addMessageToChatList(message, type = 'text') {
+    //Функция возвращает дату в нужном формате
+    dateNow() {
+        let a = new Date();
+        return `${a.getDate()}-${a.getMonth() < 10 ? 0 + (a.getMonth() + 1).toString() : a.getMonth() + 1}-${a.getFullYear()} ${a.getHours() < 10 ? 0 + a.getHours().toString() : a.getHours()}:${a.getMinutes() < 10 ? 0 + a.getMinutes().toString() : a.getMinutes()}`;
+    }
+
+    //Добавить сообщение в интерфейс пользователя
+    addMessageToChatList(message, type = 'text', data = '') {
         this.chatList.insertAdjacentHTML('beforeend', `
         <li class="chat__item">
         <div class="chat__item-content">${message}</div>  
@@ -67,17 +95,9 @@ export default class Chat {
         this.scrollDown();
     }
 
-    getTenMessages() {
-        let first = 0;
-        return ajax.getJSON(`http://localhost:3000/all?first=${first}`)
-            .pipe(
-                catchError(err => {
-                    console.log(err);
-                    return EMPTY;
-                }));
-    }
-    getSearchMessages(text) {
-        return ajax.getJSON('http://localhost:3000/messages?searchtext=' + text)
+    //Получить 10 сообщений в массиве начиная с указанной позиции
+    getTenMessages(first = 0,searchText='') {
+        return ajax.getJSON(`http://localhost:3000/messages?first=${first}&searchtext=${searchText}`)
             .pipe(
                 catchError(err => {
                     console.log(err);
@@ -85,16 +105,75 @@ export default class Chat {
                 }));
     }
 
-    renderMessages(messages) {
-        this.chatList.innerHTML = '';
-        messages.forEach(message=>{
-            this.chatList.insertAdjacentHTML('beforeend', `
+    //Отправить сообщение на сервер
+    addMessageToServer(message, type = 'text', data = '') {
+        ajax({
+            url: 'http://localhost:3000/add',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                type: type,
+                message: message,
+                date: this.dateNow(),
+                data: data
+            }
+        }).subscribe(res => {
+            if (res.response.ok) {
+                this.renderMessages([{
+                    type: type,
+                    message: message,
+                    date: this.dateNow(),
+                    data: data
+                }], true, 'beforeend')
+            }
+        })
+    }
+
+    //Получение с сервера сообщений содержащих искомую строку
+    // getSearchMessages(text) {
+    //     return ajax.getJSON('http://localhost:3000/messages?searchtext=' + text)
+    //         .pipe(
+    //             catchError(err => {
+    //                 console.log(err);
+    //                 return EMPTY;
+    //             }));
+    // }
+
+    //Функция перемотки интерфейса вниз до самого свежего сообщения
+    scrollDown() {
+        this.chatListArea.scrollTop = this.chatListArea.scrollHeight - this.chatListArea.clientHeight;
+    }
+
+    //Отрисовка массива сообщений в интерфейсе
+    renderMessages(messages, scroll = true, position = 'afterbegin', clear = false) {
+        const oldScroll = this.chatListArea.scrollHeight;
+        if (clear) this.chatList.innerHTML = '';
+
+        messages.forEach(message => {
+            let data = '';
+            if (message.type.match('image') && message.data != '') {
+                data = `<img src="${message.data}" class="chat__item-img"/>`
+            }
+            if (message.type.match('audio') && message.data != '') {
+                data = `<audio src="${message.data}" controls class="chat__item-audio"/>`
+            }
+            if (message.type.match('video') && message.data != '') {
+                data = `<video controls class="chat__item-video"><source src="${message.data}" /></video>`
+            }
+            this.chatList.insertAdjacentHTML(position, `
             <li class="chat__item">
-            <div class="chat__item-content">${message.message}</div>  
+            <div class="chat__item-content"><p class="chat__item-text">${message.message}</p>${data}</div>  
             <div class="chat__item-time">${message.date}</div></li>`);
         });
-        
-        this.scrollDown();
+        const newScroll = this.chatListArea.scrollHeight;
+        if (scroll) {
+            this.scrollDown();
+        } else {
+            document.dispatchEvent(new Event('mouseup'));
+            this.chatListArea.scrollTop = newScroll - oldScroll;
+        }
     }
 
 }
